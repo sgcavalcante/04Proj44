0ed5c6c35a169c72b8544d7d24dc7e89acebc2ac
eu <sgcdani@gmail.com>
2023-11-15 20:33:42 -0300

    .

--- apps/CadastroUsuario/views.py
+++ apps/CadastroUsuario/views.py
@@ -82,7 +82,7 @@ def cadastrar_paciente(request):
     else:
         form = CadastroPacientesForm()
      
-    return render(request,'Cadastro/cadastrar_pacientes.html')
+    return render(request,'Cadastro/Cadastrar_Cacientes.html')
 
 def listar_dados(request):
     pacientes = CadastroPacientes.objects.all()
