3b0e770a2207a3b693a6859da6e9adb0f73d1341
eu <sgcdani@gmail.com>
2024-09-18 07:43:16 -0300

    atualização

--- apps/consultas/urls.py
+++ apps/consultas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from apps.consultas.views import agendar_consulta,consultas
+from apps.consultas.views import agendar_consulta,calendario_consultas,eventos_consultas#consultas
 
 urlpatterns = [
     path('agendar_consulta/<int:paciente_id>/', agendar_consulta, name='agendar_consulta'),
-    path('consultas/', consultas, name='consultas'),
+    #path('consultas/', consultas, name='consultas'),
+    path('calendario/', calendario_consultas, name='calendario_consultas'),  # Página do calendário
+    path('eventos/', eventos_consultas, name='eventos_consultas'),  # Endpoint para os dados do calendário
 ]
--- apps/consultas/views.py
+++ apps/consultas/views.py
@@ -16,15 +16,39 @@ def agendar_consulta(request, paciente_id):
             consulta = form.save(commit=False)
             consulta.paciente = paciente  # Associa o paciente à consulta
             consulta.save()
-            return redirect('consultas')  # Redireciona para a lista de pacientes após o agendamento
+            return redirect('calendario_consultas')  # Redireciona para a lista de pacientes após o agendamento
     else:
         form = ConsultaForm()
 
     return render(request, 'agendar_consulta.html', {'form': form, 'paciente': paciente})
 
-
+'''
 @login_required
 def consultas(request):
     consultas = Consulta.objects.filter()
     return render(request, 'lista_consultas.html', {'consultas': consultas})
 
+'''
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Consulta
+from apps.CadastroUsuario.models import CadastroPacientes
+import datetime
+
+@login_required
+def calendario_consultas(request):
+    return render(request, 'lista_consultas.html')  # Template com o calendário
+
+@login_required
+def eventos_consultas(request):
+    consultas = Consulta.objects.all()
+    eventos = []
+    
+    for consulta in consultas:
+        eventos.append({
+            'title': f'Consulta: {consulta.paciente.nome}',  # Exibe o nome do paciente
+            'start': consulta.data_horario.isoformat(),  # Data e hora de início
+            'description': consulta.descricao,  # Descrição da consulta
+        })
+    
+    return JsonResponse(eventos, safe=False)  # Retorna os eventos em formato JSON
